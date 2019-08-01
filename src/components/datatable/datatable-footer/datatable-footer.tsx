23c4ab9cbd264c80f4ae4d4179ae50c75603ac35
Erwann Legeay <legeay.erwann@gmail.com>
2019-08-01 08:41:24 +0200

    Adding datatable action to handle footer actions in datatable without… (#27)
    feature: new materials-datatable-action to handle datatable footer actions

--- src/components/datatable/datatable-footer/datatable-footer.tsx
+++ src/components/datatable/datatable-footer/datatable-footer.tsx
@@ -1,4 +1,4 @@
-import { Component, h, Prop } from '@stencil/core';
+import { Component, Element, h, Prop } from '@stencil/core';
 
 @Component({
   tag: 'materials-datatable-footer',
@@ -10,10 +10,23 @@ export class DatatableFooter {
 
   @Prop() color: 'primary' | 'secondary';
 
+  @Element() host: HTMLMaterialsDatatableFooterElement;
+
+  private actions: HTMLMaterialsDatatableActionElement[];
+
+  componentWillLoad() {
+    this.actions = Array.from(this.host.querySelectorAll('materials-datatable-action'));
+  }
+
   render() {
     return (
       <div class="container">
+        {this.actions ? this.actions.map(a => {
+          return a.display && <materials-button raised color={a.color} icon={a.icon} onClick={(e) => a.press(e)}>{a.label}</materials-button>;
+        })
+        :
         <slot />
+        }
       </div>
     );
   }
